5412571f7e7099c70eb93d8c3f26e8da14598783
baafbass <98693906+baafbass@users.noreply.github.com>
2023-05-14 17:31:23 +0300

    CRUD operations are made for Ürünler

--- UrunStokTakip/Controllers/UrunController.cs
+++ UrunStokTakip/Controllers/UrunController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using UrunStokTakip.Models;
@@ -16,6 +19,76 @@ namespace UrunStokTakip.Controllers
         {
             var List = db.Urun.ToList();
             return View(List);
+        }
+        public ActionResult Ekle()
+        {
+            List<SelectListItem> deger1 = (from x in db.Kategori.ToList()
+
+                                           select new SelectListItem
+                                           {
+                                               Text = x.Ad,
+                                               Value = x.Id.ToString()
+                                           }).ToList();
+            ViewBag.ktgr = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(Urun data, HttpPostedFileBase File)
+        {
+            string path = Path.Combine("~/Content/Image" + File.FileName);
+            File.SaveAs(Server.MapPath(path));
+            data.Resim = File.FileName.ToString();
+            db.Urun.Add(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id =1)
+        {
+            var urun = db.Urun.Where(x => x.Id == id).FirstOrDefault();
+            db.Urun.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Guncelle(int id=1)
+        {
+            var GuncelUrun = db.Urun.Where(x => x.Id == id).FirstOrDefault();
+            List<SelectListItem> deger1 = (from x in db.Kategori.ToList()
+
+                                           select new SelectListItem
+                                           {
+                                               Text = x.Ad,
+                                               Value = x.Id.ToString()
+                                           }).ToList();
+            ViewBag.ktgr = deger1;
+            return View(GuncelUrun);
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Urun model,HttpPostedFileBase File)
+        {
+            var urun = db.Urun.Find(model.Id);
+            if (File==null)
+            {
+                urun.Ad = model.Ad;
+                urun.Aciklama = model.Aciklama;
+                urun.Fiyat = model.Fiyat;
+                urun.Stok = model.Stok;
+                urun.Populer = model.Populer;
+                urun.KategoriId = model.KategoriId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                urun.Resim = File.FileName.ToString();
+                urun.Ad = model.Ad;
+                urun.Aciklama = model.Aciklama;
+                urun.Fiyat = model.Fiyat;
+                urun.Stok = model.Stok;
+                urun.Populer = model.Populer;
+                urun.KategoriId = model.KategoriId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
         }
     }
 }
