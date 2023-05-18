e8ee24cf7a2e8a3acadfeec7cd86656cafcec4f0
baafbass <98693906+baafbass@users.noreply.github.com>
2023-05-18 21:25:53 +0300

    Tümünü Satın Alma işlemleri bitti

--- UrunStokTakip/Controllers/SatisController.cs
+++ UrunStokTakip/Controllers/SatisController.cs
@@ -61,5 +61,63 @@ namespace UrunStokTakip.Controllers
             }
             return View("islem");
         }
+        public ActionResult HepsiniSatinAl(decimal? Tutar)
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                var kullaniciadi = User.Identity.Name;
+                var kullanici = db.Kullanici.FirstOrDefault(x => x.Email == kullaniciadi);
+                var model = db.Sepet.Where(x => x.KullaniciId == kullanici.Id).ToList();
+                var kid = db.Sepet.FirstOrDefault(x => x.KullaniciId == kullanici.Id);
+                if(model!=null)
+                {
+                    if(kid==null)
+                    {
+                        ViewBag.Tutar = "Sepetinizde Ürün Bulunmamaktadır";
+                    }
+                    else if (kid != null)
+                    {
+                        Tutar = db.Sepet.Where(x => x.KullaniciId == kid.KullaniciId).Sum(x => x.Urun.Fiyat * x.Adet);
+                        ViewBag.Tutar = "Toplam Tutar =" + Tutar + "TL";
+                    }
+                    return View(model);
+                }
+                return View();
+            }
+            return HttpNotFound();
+        }
+
+        [HttpPost]
+        public ActionResult HepsiniSatinAl2()
+        {
+            var username = User.Identity.Name;
+            var kullanici = db.Kullanici.FirstOrDefault(x => x.Email == username);
+            var model = db.Sepet.Where(x => x.KullaniciId == kullanici.Id).ToList();
+            int satir = 0;
+           
+            foreach(var item in model)
+            {
+                var satis = new Satislar
+                {
+                    kullaniciId = model[satir].KullaniciId,
+                    UrunId = model[satir].UrunId,
+                    Adet = model[satir].Adet,
+                    Fiyat = model[satir].Fiyat,
+                    Resim = model[satir].Urun.Resim,
+                    Tarih = DateTime.Now
+                };
+                db.Satislar.Add(satis);
+                db.SaveChanges();
+                satir++;
+            }
+            db.Sepet.RemoveRange(model);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Sepet");
+        }
+
+
+
+
+
     }
 }
