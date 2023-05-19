d2caa43f2f1615ba9a0324e1e98ed9e7de70c9ef
baafbass <98693906+baafbass@users.noreply.github.com>
2023-05-19 14:28:03 +0300

    Istatistikler controller is created and the tumSatislar one

---
+++ UrunStokTakip/Controllers/IstatistikController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunStokTakip.Models;
+
+namespace UrunStokTakip.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Takip_SistemiEntities2 db = new Takip_SistemiEntities2();
+
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            var satis = db.Satislar.Count();
+            ViewBag.satis = satis;
+            var urun = db.Urun.Count();
+            ViewBag.urun = urun;
+            var kategori = db.Kategori.Count();
+            ViewBag.Kategori = kategori;
+            var sepet = db.Sepet.Count();
+            ViewBag.sepet = sepet;
+            var kullanici = db.Kullanici.Count();
+            ViewBag.kullanici = kullanici;
+            return View();
+        }
+    }
+}
