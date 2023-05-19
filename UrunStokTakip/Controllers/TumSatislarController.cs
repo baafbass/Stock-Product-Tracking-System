7705f488418ee36ed404b8f86a694c5614017e0e
baafbass <98693906+baafbass@users.noreply.github.com>
2023-05-19 15:16:24 +0300

    Tum Satislar Controller is created

--- UrunStokTakip/Controllers/TumSatislarController.cs
+++ UrunStokTakip/Controllers/TumSatislarController.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UrunStokTakip.Models;
 
 namespace UrunStokTakip.Controllers
 {
     public class TumSatislarController : Controller
     {
+        Takip_SistemiEntities2 db = new Takip_SistemiEntities2();
+
         // GET: TumSatislar
-        public ActionResult Index()
+        public ActionResult Index(int sayfa=1)
         {
-            return View();
+            return View(db.Satislar.ToList().ToPagedList(sayfa,5)) ;
         }
     }
 }
